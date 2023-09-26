4af9c6d7b3051f6f48fb9dc877ac609dca85c5f5
Fernando Costa <fernando.costa@ironhack.com>
2023-09-27 00:35:24 +0200

    uploading

--- databases/erasmus.sql
+++ databases/erasmus.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:026eef561db6dff57ea4d61970751cd6a1a0898bdd1c8f85fdc0e1a3e0c9513b
-size 82398
+oid sha256:eef4dabab5caf72efd0da6a57b55dc041327c709bbb47f979769c1c1a18f74c8
+size 63467
